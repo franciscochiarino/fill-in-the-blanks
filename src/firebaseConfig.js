72b3993addfb28ad804de5d5ae5b71e04df1a86a
Fran <francisco.chiarino@gmail.com>
2023-06-11 14:29:11 +0200

    env variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPn6aHW-T3Q3fPtajoLOSsJnjxxX0SD8o",
-  authDomain: "fill-in-the-blanks-86d07.firebaseapp.com",
-  projectId: "fill-in-the-blanks-86d07",
-  storageBucket: "fill-in-the-blanks-86d07.appspot.com",
-  messagingSenderId: "1024101598440",
-  appId: "1:1024101598440:web:bcc49e44d585029baff55d"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
