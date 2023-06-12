c79c601536833aafebe901bd037cade0606dbfc4
Fran <francisco.chiarino@gmail.com>
2023-06-12 10:34:47 +0200

    add github link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/molecules/Navbar';
+import Footer from './components/molecules/Footer';
 import Exercises from './components/pages/Exercises';
 import Admin from './components/pages/Admin';
 
 const App = () => {
   return (
-    <div className="has-background-white mb-6">
+    <div className="has-background-white">
       <Navbar />
 
       <div className="columns">
@@ -18,6 +19,8 @@ const App = () => {
           </Routes>
         </div>
       </div>
+
+      <Footer />
     </div>
   )
 }
---
+++ src/components/molecules/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <a href="https://github.com/franciscochiarino/fill-in-the-blanks" target="_blank" rel="noreferrer">
+          <button class="button">
+            <span class="icon">
+              <i class="fab fa-github"></i>
+            </span>
+            <span>GitHub</span>
+          </button>
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
